50ef10bb105209ac5abaa3df39e71ea14d3d378e
misaelBullon <misa.esteban@gmail.com>
2020-08-17 17:59:32 -0300

    Piedra papel o tijera con manipulacion del dom

---
+++ index.js
@@ -0,0 +1,79 @@
+
+const piedra = document.getElementById('piedra');
+const papel = document.getElementById('papel');
+const tijera = document.getElementById('tijeras');
+
+
+//Funcion de la eleccion de la computadora
+function computerSelection(){
+    var election = (Math.random().toPrecision(1)) * 10;
+    var newElection = Math.round(election);
+    var cadena;
+    //console.log(newElection);
+
+
+    if(newElection <= 3){
+        cadena = 'piedra';
+        //console.log(cadena);
+        return cadena;
+    }else if(newElection > 3 && newElection <= 6){
+        cadena = 'papel';
+        //console.log(cadena);
+        return cadena;
+    }else if(newElection > 6 && newElection <= 10){
+        cadena = 'tijera';
+        //console.log(cadena);
+        return cadena;
+    }
+
+}
+function oneround(player, computer){
+
+    var jugador = player;
+    var computadora = computer; 
+    var j = document.getElementById('jugador');
+    var c = document.getElementById('computadora');
+    var g = document.getElementById('ganador');
+
+    j.textContent = 'Jugador: ' + jugador;
+    c.textContent = 'Computadora: ' + computadora;
+
+    if(jugador === computadora){
+        g.textContent = 'Es un empate';
+    }else if(jugador == 'piedra' && computadora == 'tijera' || jugador == 'papel' && computadora == 'piedra' || jugador == 'tijera' && computadora == 'papel'){
+        g.textContent = 'Has ganado';
+        return 'j';
+    }else{
+       g.textContent = 'Ha ganado la computadora';
+       return 'c';
+    }
+}
+
+
+
+function jugar(){
+
+
+    piedra.addEventListener('click', function(){
+        var jugador = piedra.getAttribute('value');
+        var computadora = computerSelection();
+        var roundResult = oneround(jugador, computadora);
+        console.log(roundResult);
+    });
+    
+    papel.addEventListener('click', function(){
+        var jugador = papel.getAttribute('value');
+        var computadora = computerSelection();
+        var roundResult = oneround(jugador, computadora);
+        console.log(roundResult);
+    });
+
+    tijera.addEventListener('click', function(){
+        var jugador = tijera.getAttribute('value');
+        var computadora = computerSelection();
+        var roundResult = oneround(jugador, computadora);
+        console.log(roundResult);
+    });
+}
+
+jugar();
